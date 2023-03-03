bc4135f42d747242e811039de1024697c3ac172a
sagiv23 <104361570+sagiv23@users.noreply.github.com>
2023-03-03 17:31:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+var redB = document.getElementById("red");
+var blueB = document.getElementById("blue");
+var blackB = document.getElementById("black");
+var main1 = document.getElementById("iPhoneImage");
+
+redB.onclick = function(){
+    main1.src="red iphone.png";
+}
+
+blueB.onclick = function(){
+    main1.src="blue iphone.png";
+}
+
+blackB.onclick = function(){
+    main1.src="black iphone.png";
+}
+
+
+
+
+
+
+var bordoB = document.getElementById("bordo");
+var greenB = document.getElementById("green");
+var whiteB = document.getElementById("white");
+var main2 = document.getElementById("samsungImage");
+
+bordoB.onclick = function(){
+    main2.src="samsung 22 bordo.png";
+}
+
+greenB.onclick = function(){
+    main2.src="samsung 22 green.png";
+}
+
+whiteB.onclick = function(){
+    main2.src="samsung 22 white.png";
+}
+
+
+
+
+
+var coalB = document.getElementById("coal");
+var fjordB = document.getElementById("fjord");
+var grayB = document.getElementById("gray");
+var main3 = document.getElementById("NokiaImage");
+
+coalB.onclick = function(){
+    main3.src="nokia 2.4 coal.png";
+}
+
+fjordB.onclick = function(){
+    main3.src="nokia 2.4 fjord.png";
+}
+
+grayB.onclick = function(){
+    main3.src="nokia 2.4 gray.png";
+}
+
+
+
+
+
+var blacklB = document.getElementById("black2");
+var cyanB = document.getElementById("cyan");
+var whiteB = document.getElementById("white2");
+var main4 = document.getElementById("XiaomiImage");
+
+blacklB.onclick = function(){
+    main4.src="xiaomi 12 ultra black.png";
+}
+
+cyanB.onclick = function(){
+    main4.src="xiaomi 12 ultra cyan.png";
+}
+
+whiteB.onclick = function(){
+    main4.src="xiaomi 12 ultra white.png";
+}
